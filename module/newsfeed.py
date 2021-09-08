77cb12fce9451f8f0e2399e7bd3a4907832149c0
ted111712 <ted111712@gmail.com>
2021-09-08 23:17:40 +0900

    코드 정리

--- module/newsfeed.py
+++ module/newsfeed.py
@@ -1,16 +1,18 @@
-# -*- coding: utf-8 -*-
 import requests
 import xmltodict # 따로 설치
 import json
 
 def get_newsfeed():
-    url = "https://www.yonhapnewstv.co.kr/category/news/headline/feed/"
+    url = "https://www.yonhapnewstv.co.kr/category/news/headline/feed/" # 연합뉴스 헤드라인 RSS 이용
     res = requests.get(url)
     xml = xmltodict.parse(res.text)
     dict1 = json.loads(json.dumps(xml))
     dict_data = dict1["rss"]["channel"]["item"]
+
+    # 헤드라인 리스트 생성
     news_headlines = []
     
+    # 리스트에 헤드라인 추가
     for n in range(len(dict_data)):
         news_headlines.append(dict_data[n]['title'])
 
