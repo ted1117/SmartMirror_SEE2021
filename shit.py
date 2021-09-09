45b85f02a9f8ae72d93746fb8f5d83e983508fb3
ted111712 <ted111712@gmail.com>
2021-09-09 15:24:30 +0900

    upload backup

--- shit.py
+++ shit.py
@@ -2,14 +2,15 @@ from tkinter import *
 from tkinter import ttk
 import tkinter
 import time
-from module import newsfeed, weather_api
+from PIL import Image, ImageTk
+from module import newsfeed, weather_api, sunsetrise_api
 
 # 속성
 root = tkinter.Tk()
 root.title("Smart Mirror")
 root.geometry("1920x1080")
-root.configure(background="black")
-root.resizable(1, 1)
+#root.configure(background="black")
+root.resizable(True, True)
 
 class Board(Frame):
     cnt = 0
@@ -20,27 +21,29 @@ class Board(Frame):
         self.pack(expand=True, fill=BOTH)
         
         # 글꼴
-        self.largeFont = ("맑은 고딕", 68)
+        self.largeFont = ("맑은 고딕", 80)
         self.middleFont = ("맑은 고딕", 45)
         self.normalFont = ("맑은 고딕", 30)
 
         # 시간/날짜
-        self.time_label = ttk.Label(self, font=self.largeFont, background="black", foreground="white")
-        self.time_label.pack(side="top")
-        self.date_label = ttk.Label(self, font=self.normalFont, background="black", foreground="white")
-        self.date_label.pack(side="top")
+        clock_frame = Frame(self, bg="black")
+        clock_frame.pack(side="top", fill="both")
+        self.time_label = ttk.Label(clock_frame, font=self.largeFont, background="black", foreground="white")
+        self.time_label.pack()
+        self.date_label = ttk.Label(clock_frame, font=self.normalFont, background="black", foreground="white")
+        self.date_label.pack()
 
         # 뉴스
         news_frame = Frame(self, height=300, bg='black')
         news_frame.pack(side="bottom", fill="both")
-        self.news_label = ttk.Label(news_frame, font=self.middleFont, background="pink", foreground="white")
+        self.news_label = ttk.Label(news_frame, font=self.middleFont, background="black", foreground="white")
         self.news_label.pack(anchor="center")
 
         # 날씨
         weather_frame = Frame(self, bg='red')
         weather_frame.pack(side="left", fill="y")
-        self.weather_label = ttk.Label(weather_frame, font=self.largeFont, background="black", foreground="white")
-        self.weather_label.pack(anchor="center")
+        self.tempnow_label = ttk.Label(weather_frame, font=self.largeFont, background="black", foreground="white")
+        self.tempnow_label.pack(side="right")
 
         # 일정
         schedule_frame = Frame(self, bg='blue')
@@ -58,8 +61,8 @@ class Board(Frame):
 
     # 날씨 기능
     def updateWeather(self):
-        self.weather_label.configure(text=time.strftime("%H:%M:%S"))
-        self.weather_label.after(200, self.updateWeather)
+        self.tempnow_label.configure(text=weather_api.get_weather()["temp"][0] + "°C")
+        self.tempnow_label.after(600000, self.updateWeather)
 
     # 뉴스 기능
     def updateNews(self):
